f7a129165d852f534abdff2471214da3e3e79330
Dhruvin Pambhar <dhruvinpambhar0609@gmail.com>
2024-06-08 13:32:19 +0530

    final commit

---
+++ DotNetCoreMVCProject/eCar-Rental-System/eCar-Rental-System/Migrations/20231023131705_car_rantel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCar_Rental_System.Migrations
+{
+    public partial class car_rantel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
